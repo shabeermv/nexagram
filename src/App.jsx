03ca2235042d643382d97488866875f0854d32f3
Shabeer Ali <shabeermv740@gmail.com>
2026-09-08 17:16:23 +0530

    edited nodemailer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import React from "react";
+
 import { Navbar, Footer, FixedPlugin } from "@/components";
 
 import Hero from "@/app/hero";
 import OnlineCourse from "@/app/about";
-import Services from "./app/services";
-
+import Services from "@/app/services";
 import Founders from "@/app/founders";
 import Brands from "@/app/brands";
-import OtherCourses from "@/app/achivements";
+import Achivements from "@/app/achivements";
 import Contact from "@/app/contact";
 
 export function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
+
       <main>
         <Hero />
         <OnlineCourse />
         <Services />
-
         <Founders />
         <Brands />
-        <OtherCourses />
+        <Achivements />
         <Contact />
       </main>
+
       <Footer />
       <FixedPlugin />
     </div>
